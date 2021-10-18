3d197070b989f68b1f462a45d8a717d4c61a95c3
sagar <7208sk@gmail.com>
2021-10-18 15:20:25 +0530

    Creating Master Branch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericMaximumProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
