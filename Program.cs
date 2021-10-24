dd4ac21f112c05225a482c7f638c08e49335074e
sagar <7208sk@gmail.com>
2021-10-24 18:40:28 +0530

    Finding maximum String

--- Program.cs
+++ Program.cs
@@ -4,11 +4,12 @@ namespace GenericMaximumProblem
 {
 
 
+
     public class Program
     {
 
 
-        public float FindMaximumFloat(float first, float second, float third)
+        public string FindMaximumString(string first, string second, string third)
         {
             if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
             {
@@ -25,16 +26,26 @@ namespace GenericMaximumProblem
             else
             {
                 Console.WriteLine("All three having same number");
-                return -1;
+                return default;
             }
         }
+
+
 
         static void Main(string[] args)
         {
             Program obj = new Program();
-            Console.WriteLine("Maximum value is : " + obj.FindMaximumFloat(70.2f, 45.3f, 12.5f));
+            Console.WriteLine("Maximum value is : " + obj.FindMaximumString("Tata", "BMW", "RX"));
 
             Console.ReadLine();
         }
+
+
     }
+
+
+
+
+
+
 }
