ee17fa02341cd1ee5509cede9ae1a65bf415d7fb
sagar <7208sk@gmail.com>
2021-10-24 19:05:21 +0530

    shorting

--- Program.cs
+++ Program.cs
@@ -3,49 +3,12 @@
 namespace GenericMaximumProblem
 {
 
-
-
-    public class Program
+    class Program
     {
-
-
-        public string FindMaximumString(string first, string second, string third)
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                return first;
-            }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                return second;
-            }
-            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
-            {
-                return third;
-            }
-            else
-            {
-                Console.WriteLine("All three having same number");
-                return default;
-            }
-        }
-
-
-
         static void Main(string[] args)
         {
-            Program obj = new Program();
-            Console.WriteLine("Maximum value is : " + obj.FindMaximumString("Tata", "BMW", "RX"));
+            Console.WriteLine("Welcome to Generics Problem!!!");
 
-            Console.ReadLine();
         }
-
-
     }
-
-
-
-
-
-
 }
